6915ecbf45caea02b619c2aa8d3effc1365cf698
Hamed <hamed@Sherrys-MacBook.lan>
2016-01-29 00:33:47 +0100

    Gulp working.

--- gulpFile.js
+++ gulpFile.js
@@ -4,30 +4,61 @@ var postcss = require('gulp-postcss');
 var nested = require('postcss-nested');
 var variables = require('postcss-css-variables');
 var autoprefixer = require('autoprefixer');
+var watch = require('gulp-watch');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var plumber = require('gulp-plumber');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+var concatCss = require('gulp-concat-css');
 
 gulp.task('css', function () {
     var processors = [nested, variables, autoprefixer];
     return gulp
         .src('./app/**/*.css')
+        .pipe(plumber())
         .pipe(postcss(processors))
-        .pipe(gulp.dest('./wwwroot/css/'));
+        .pipe(sourcemaps.init())
+        .pipe(gulp.dest('./wwwroot/css'))
+        .pipe(minifyCss())
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest('./wwwroot/css/'))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(gulp.dest('./wwwroot/css'));
+});
+
+gulp.task('css:watch', ['css'], function () {
+    gulp.watch('./app/**/*.css', ['css']);
+});
+
+gulp.task('js', function () {
+    return gulp
+        .src('./app/**/*.js')
+        .pipe(plumber())
+        .pipe(gulp.dest('./wwwroot/js'))
+        .pipe(sourcemaps.init())
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest('./wwwroot/js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(gulp.dest('./wwwroot/js'));
+});
+
+gulp.task('js:watch', ['js'], function () {
+    gulp.watch('./app/**/*.js', ['js']);
 });
 
 gulp.task('copy', function () {
     gulp
         .src('./app/**/*.html')
-        .pipe(gulp.dest('./wwwroot/'));
-    gulp
-        .src('./app/**/*.js')
-        .pipe(gulp.dest('./wwwroot/js/'));
+        .pipe(gulp.dest('./wwwroot'));
 });
 
 gulp.task('copy:watch', ['copy'], function () {
-    gulp.watch('./app/**/*.{html,js}', ['copy']);
-});
-
-gulp.task('css:watch', ['css'], function () {
-    gulp.watch('./app/**/*.css', ['css']);
+    gulp.watch('./app/**/*.html', ['copy']);
 });
 
 gulp.task('default', []);
---
+++ wwwroot/js/any.js
@@ -0,0 +1,47 @@
+(function () {
+    var $ = window.$;
+    console.log('[red]', 'started');
+    
+    var debugInfo = $('debug-info');
+    
+    var doc = $(document);
+    var body = $('body');
+    var win = $(window);
+    
+    var verseList = $('.verse');
+    
+    
+    verseList.each(function(index){
+        var verse = $(this);
+        
+        var position = verse.position();
+        
+        console.log(position.top);
+    });
+    
+
+
+    win.on('mousewheel', touchMove);
+    var viewport = win.height();
+    var wheel = 0;
+    var verse = $('.verse').first();
+    function touchMove(event, delta) {
+        var position = verse.offset();
+        var bodyPosition = body.offset();
+        debugInfo.prepend('[book][verse-position-top]', position.top, '<br>');
+        debugInfo.prepend('[book][body-position-top]', bodyPosition.top, '<br>');
+        debugInfo.prepend('[book][window-height]', viewport, '<br>');
+        debugInfo.prepend('[book][doc-height]', doc.height(), '<br>');
+        var currentMatrix = verse.css('transform').split('(')[1].split(')')[0].split(', ');
+        var currentPosition = parseInt(currentMatrix[4]);
+        
+        wheel = wheel + currentPosition + Math.floor(event.originalEvent.wheelDelta);
+        
+        verse.css('transform', 'translateY('+ wheel +'px');
+        
+        debugInfo.append('[book][touch-move]', wheel);
+        
+    }
+    
+
+} ());
