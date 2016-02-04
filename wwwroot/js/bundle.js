edbd443495b4a360ee084dc51c080aba6ddb4c0d
Hamed <hamed@Sherrys-MacBook.lan>
2016-02-04 03:04:15 +0100

    diffrent touch events

--- wwwroot/js/bundle.js
+++ wwwroot/js/bundle.js
@@ -1,60 +1,40 @@
 (function () {
-   
-    
-    var debugInfo = $('debug-info');
-    
-    var doc = $(document);
-    var body = $('body');
-    var win = $(window);
-    
-    var verseList = $('.verse');
-    
-    
-    verseList.each(function(index){
-        var verse = $(this);
-        
-        var position = verse.position();
-        
-        console.log(position.top);
-    });
-    
-    // win.on('touchstart', function(event){
-    //     event.preventDefault();
-    //     iio.log('touch start');
-    //     win.on('touchmove', function(event){
-    //         iio.log('touch move');
-    //         iio.log(event.originalEvent.touches[0].pageX);
-    //     });
-    // });
-    
 
-    win.on('mousewheel', touchMove);
-    var viewport = win.height();
-    var wheel = 0;
-    var verse = $('.verse');
-    var firstVerse = $('.verse').first();
-    function touchMove(event, delta) {
-        var position = verse.offset();
-        var bodyPosition = body.offset();
-        
-        debugInfo.prepend('[book][verse-position-top]', position.top, '<br>');
-        debugInfo.prepend('[book][body-position-top]', bodyPosition.top, '<br>');
-        debugInfo.prepend('[book][window-height]', viewport, '<br>');
-        debugInfo.prepend('[book][doc-height]', doc.height(), '<br>');
-        
-        var currentMatrix = verse.css('transform').split('(')[1].split(')')[0].split(', ');
-        var currentPosition = parseInt(currentMatrix[4]);
-        
-        wheel = wheel + currentPosition + Math.floor(event.originalEvent.wheelDelta);
-        
-        // verse.css('transform', 'translateZ('+wheel/-2+'px) translateY('+ wheel +'px) rotateX('+ wheel +'deg)');
-        verse.css('transform', 'translateY('+ wheel*-1 +'px)');
-        firstVerse.css('transform', 'rotateX('+ wheel*.9 +'deg) translateY('+ wheel*-1.1 +'px)');
-        
-        console.log('[book][touch-move]', wheel);
-        
-    }
-    
+    var $ = window.$;
+// 
+//     var win = $('body');
+//     var verses = $('#verses');
+// 
+//     win.on('touchstart', touchStart);
+//     function touchStart(event) {
+//         event.preventDefault();
+//         console.log('touch start');
+//         
+//         // event.targetTouches[0].target.style.webkitTransform
+//     }
+//     var currentMatrix = verses.css('transform').split('(')[1].split(')')[0].split(', ');
+//     var currentPosition = parseInt(currentMatrix[5]);
+//     var offset = event.originalEvent.touches[0].pageY;
+
+
+    document.addEventListener('touchmove', touchMove);
+    function touchMove(event) {
+        event.preventDefault();
+        console.log(event.changedTouches[0].pageY);
+        console.log(event.targetTouches[0].screenY);
+        console.log(event.touches[0].pageY);
+        console.log('pageY', event.targetTouches[0].pageY);
+//         console.log('pageY', event.targetTouches[0].pageY); 
+//         // 
+//         var moveTo = 'translateY(' + (currentPosition + event.targetTouches[0].pageY - offset) + 'px)';
+//         // console.log('moveTo: ', moveTo); 
+//         // var moveTo = 'translate(' + (currentPosition + ((event.clientX) - offset)) + 'px)';
+// 
+//         // self.css('transform', moveTo);
+//         // itemsTwins.css('transform', moveTo);
+//         verses.css('-webkit-transform', moveTo);
+
+    };
 
 } ());
 (function () {
